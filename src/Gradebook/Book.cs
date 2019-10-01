01db2fc134740dbd8d605a2bf3dfbb7d5d3da36b
mikesnacki <mikesnacki@gmail.com>
2019-10-01 04:38:03 -0400

    unit testing

---
+++ src/Gradebook/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gradebook
+{
+    class Book
+    {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            double result = 0.0;
+            double average;
+            double highGrade = double.MinValue;
+            double lowGrade = double.MaxValue;
+
+            foreach (double grade in grades)
+            {
+                result += grade;
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+            }
+            average = result / grades.Count;
+
+            Console.WriteLine($"The lowest is {lowGrade}, the average is {average} the highest is {highGrade}");
+        }
+
+    }
+}
---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Mike's book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.AddGrade(56.1);
+            book.ShowStatistics();
+        }
+    }
+
+    
+}
