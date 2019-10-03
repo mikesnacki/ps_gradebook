b0576a178bf2b2484f54976c5fc81631f56674dd
mikesnacki <mikesnacki@gmail.com>
2019-10-03 06:02:00 -0400

    exception handling added

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -15,9 +15,37 @@ namespace Gradebook
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade >= 0 && grade <= 100) 
+            {
+                grades.Add(grade);
+                Console.WriteLine($"Grade of {grade} added");
+            }
+            else 
+            {
+                throw new ArgumentException("$Invalid {nameof(grade)}");
+                Console.WriteLine("Invalid grade");
+            }
         }
 
         public Statistics GetStatistics()
@@ -35,6 +63,25 @@ namespace Gradebook
             }
 
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;                
+                default:
+                    result.Letter = 'F';
+                    break;      
+            }
 
             return result;
         }
--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -8,13 +8,37 @@ namespace Gradebook
         static void Main(string[] args)
         {
             var book = new Book("Mike's book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
-            book.AddGrade(56.1);
+            bool running = true;
+
+            while(true)
+            {
+                Console.WriteLine("Enter grades, type 'q' or 'quit to stop\n");
+                Console.WriteLine("Enter grade: ");
+                string resp = Console.ReadLine();
+
+                if (resp.ToLower() =="q" || resp.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                try
+                {
+                   book.AddGrade(Convert.ToDouble(resp)); 
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                    
+            }
+
             var stats = book.GetStatistics();
 
-            Console.WriteLine($"The lowest is {stats.Low}, the average is {stats.Average} the highest is {stats.High}");
+            Console.WriteLine($"The lowest is {stats.Low:N1}, \nthe average is {stats.Average:N1}, \nthe highest is {stats.High:N1}\n the letter is {stats.Letter}");
         }
     }
 
